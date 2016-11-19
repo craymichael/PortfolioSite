5d9fd608cac478858b9c2bb20ff1f6998e82bb8e
Zach Carmichael <20629897+craymichael@users.noreply.github.com>
2016-11-19 09:07:07 -0500

    Portfolio Update
    Portfolio base layout and content. Models added, but will likely be removed

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,3 +1,49 @@
 from django.db import models
 
-# Create your models here.
+
+class Page(models.Model):
+    """"""
+    name = models.CharField(max_length=50, primary_key=True, blank=True, unique=True)
+
+    def __str__(self):
+        """The page's name."""
+        return self.name
+
+
+def default_page():
+    return Page.objects.get_or_create(name='')[0]
+
+
+class Section(models.Model):
+    """Base section. Uses self for sub-sections."""
+    section_title = models.CharField(max_length=100)
+    sub_section = models.ForeignKey('self', on_delete=models.CASCADE, null=True)
+    order = models.PositiveIntegerField(default=0)
+    page = models.ForeignKey(Page, on_delete=models.CASCADE, null=True, default=default_page)
+
+    class Meta:
+        """Metadata for Section."""
+        unique_together = (
+            ('page', 'section_title'),  # Unique title for each section in page
+            ('sub_section', 'section_title'),  # Unique title for each subsection in section
+        )
+        ordering = ['order', 'section_title']  # Order by ascending order (starting at 0), then by title
+
+    def __str__(self):
+        """The section's title."""
+        return self.section_title
+
+
+class SectionItem(models.Model):
+    """Base section item. Contains text to be displayed, a section can have multiple SectionItems."""
+    section = models.ForeignKey(Section, on_delete=models.CASCADE)
+    section_text = models.TextField(max_length=10000)
+    order = models.PositiveIntegerField(default=0)
+
+    class Meta:
+        """Metadata for SectionItem."""
+        ordering = ['order', 'section_text']  # Order by ascending order (starting at 0), then by text
+
+    def __str__(self):
+        """The section item's text."""
+        return self.section_text
--- portfolio/views.py
+++ portfolio/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+from .models import Page
+
+
+class IndexView(generic.TemplateView):
+    template_name = 'portfolio/index.html'
+    active = 'index'
+
+    def get_context_data(self, **kwargs):
+        context = super(IndexView, self).get_context_data(**kwargs)
+        context['active'] = self.active
+        return context
+
+
+class PortfolioView(generic.TemplateView):
+    template_name = 'portfolio/portfolio.html'
+    context_object_name = 'page'
+
+
+class ProjectsView(generic.TemplateView):
+    template_name = 'portfolio/projects.html'
+    active = 'projects'
+
+    def get_context_data(self, **kwargs):
+        context = super(ProjectsView, self).get_context_data(**kwargs)
+        context['active'] = self.active
+        return context
+
+
+class ContactView(generic.TemplateView):
+    template_name = 'portfolio/contact.html'
+    active = 'contact'
+
+    def get_context_data(self, **kwargs):
+        context = super(ContactView, self).get_context_data(**kwargs)
+        context['active'] = self.active
+        return context
