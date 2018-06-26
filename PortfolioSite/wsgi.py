8c561560cc7d4ef58a5db6b4405ea93bf9b58f3a
Zach Carmichael <zjc2920@g.rit.edu>
2018-06-26 18:17:29 -0400

    Current site status 6.26.18

--- PortfolioSite/wsgi.py
+++ PortfolioSite/wsgi.py
@@ -9,8 +9,9 @@ https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 
 import os
 
+os.environ['DJANGO_SETTINGS_MODULE'] = 'PortfolioSite.settings'
+
+from django.contrib.auth.handlers.modwsgi import check_password
+
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "PortfolioSite.settings")
-
 application = get_wsgi_application()
