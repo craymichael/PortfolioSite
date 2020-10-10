92765e392410b4c25db6bd6230fdd7dc1dfbc1bd
Zach Carmichael <20629897+craymichael@users.noreply.github.com>
2020-10-10 16:43:35 -0400

    static files

--- PortfolioSite/settings.py
+++ PortfolioSite/settings.py
@@ -138,8 +138,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 
-STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'portfolio', 'static'),)
 
 # HTTPS
 CSRF_COOKIE_SECURE = True
